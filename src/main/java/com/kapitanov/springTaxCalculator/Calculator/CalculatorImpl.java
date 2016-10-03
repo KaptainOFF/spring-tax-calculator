94ad2890954194bbd10ca576c85934beeedbc439
Yani Kapitanov <yani.kapitanov@gmail.com>
2016-10-03 23:40:50 +0100

    Decomposed the calculator class

--- src/main/java/com/kapitanov/springTaxCalculator/Calculator/CalculatorImpl.java
+++ src/main/java/com/kapitanov/springTaxCalculator/Calculator/CalculatorImpl.java
@@ -6,129 +6,103 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CalculatorImpl implements Calculator {
-	public static final BigDecimal PERCENTAGE_TIER_ONE = new BigDecimal("0.20");
-	public static final BigDecimal PERCENTAGE_TIER_TWO = new BigDecimal("0.40");
-	public static final BigDecimal PERCENTAGE_TIER_THREE = new BigDecimal("0.45");
+	public static final BigDecimal BASIC_RATE = new BigDecimal("0.20");
+	public static final BigDecimal HIGHER_RATE = new BigDecimal("0.40");
+	public static final BigDecimal ADDITIONAL_RATE = new BigDecimal("0.45");
+	public static final BigDecimal limit = new BigDecimal("200000");
+
+	private BigDecimal basicRateStart;
+	private BigDecimal higherRateStart;
+	private BigDecimal additionalRateStart;
 
 	@Override
 	public BigDecimal netAmount(String yearEnd, BigDecimal grossAmount) {
-		BigDecimal finalAmount = new BigDecimal("0");
-		
+
+
 		if (yearEnd.equals("2014/2015")) {
+
+			basicRateStart = new BigDecimal("10000");
+			higherRateStart = new BigDecimal("41865");
+			additionalRateStart = new BigDecimal("150000");
+
+			return takeHomeHelper(basicRateStart, higherRateStart, additionalRateStart, grossAmount);
+
+		}
+
+		if (yearEnd.equals("2015/2016")) {
+
+			basicRateStart = new BigDecimal("10600");
+			higherRateStart = new BigDecimal("42385");
+			additionalRateStart = new BigDecimal("150000");
+
+			return takeHomeHelper(basicRateStart, higherRateStart, additionalRateStart, grossAmount);
+
+		} 
+		return new BigDecimal("0");
+	}
+
+	private BigDecimal takeHomeHelper(BigDecimal basicRateStart,
+			BigDecimal higherRateStart,
+			BigDecimal additionalRateStart,
+			BigDecimal grossAmount) {
+		BigDecimal finalAmount = new BigDecimal("0");
+
+		if (grossAmount.compareTo(basicRateStart) == -1) {
 			
-			BigDecimal firstTier = new BigDecimal("10000");
-			BigDecimal secondTier = new BigDecimal("41865");
-			BigDecimal thirdTier = new BigDecimal("150000");
-			BigDecimal finalTier = new BigDecimal("200000");
-			
-			if (grossAmount.compareTo(firstTier) == -1) {
-				
-				finalAmount = grossAmount;
-				
-			} else if (grossAmount.compareTo(firstTier) == 1 && 
-						(grossAmount.compareTo(secondTier) == 0 ||
-						grossAmount.compareTo(secondTier) == -1) ) {
-				
-				BigDecimal taxableAmount = grossAmount.subtract(firstTier);
-				
-				// Calculating tax if amount is less than 41,865
-				
-				BigDecimal tax =  taxableAmount.multiply(PERCENTAGE_TIER_ONE);
-				
-				finalAmount = grossAmount.subtract(tax);
-				System.out.println("Your tax is: " + tax);
-			} else if ( grossAmount.compareTo(secondTier) == 1 &&
-						(grossAmount.compareTo(thirdTier) == 0 ||
-						grossAmount.compareTo(thirdTier) == -1) ) {
-	
-				BigDecimal firstTaxableAmount = new BigDecimal("31865");
-				BigDecimal secondTaxableAmount = grossAmount.subtract(secondTier);
-				
-				
-				// Calculating tax if amount is between 41,865 and 150,000
-				
-				BigDecimal tax = ((firstTaxableAmount.multiply(
-									PERCENTAGE_TIER_ONE)).add(
-									secondTaxableAmount.multiply(
-									PERCENTAGE_TIER_TWO)));
-				
-				finalAmount = grossAmount.subtract(tax);
-				System.out.println("Your tax is: " + tax);
-			} else if ( grossAmount.compareTo(thirdTier) == 1 ) {
-				
-				BigDecimal firstTaxableAmount = new BigDecimal("31865");
-				BigDecimal secondTaxableAmount = thirdTier.subtract(secondTier);
-				BigDecimal thirdTaxableAmount = finalTier.subtract(grossAmount);
-				
-				// Calculating tax if amount is between 150,000 and 200,000
-				
-				BigDecimal tax = (firstTaxableAmount.multiply(
-								   PERCENTAGE_TIER_ONE).add(
-								   secondTaxableAmount.multiply(
-								   PERCENTAGE_TIER_TWO).add(
-								   thirdTaxableAmount.multiply(
-								   PERCENTAGE_TIER_THREE))));
-				
-				finalAmount = grossAmount.subtract(tax);
-			}
-			
-		} else if (yearEnd.equals("2015/2016")) {
-			
-			BigDecimal firstTier = new BigDecimal("10600");
-			BigDecimal secondTier = new BigDecimal("42385");
-			BigDecimal thirdTier = new BigDecimal("150000");
-			BigDecimal finalTier = new BigDecimal("200000");
-			
-			if (grossAmount.compareTo(firstTier) == -1) {
-				finalAmount = grossAmount;
-			} else if ( grossAmount.compareTo(firstTier) == 1 && 
-						(grossAmount.compareTo(secondTier) == 0 ||
-						grossAmount.compareTo(secondTier) == -1) ) {
-				
-				BigDecimal taxableAmount = grossAmount.subtract(firstTier);
-				
-				// Calculating tax if amount is less than 42,385
-				
-				BigDecimal tax =  taxableAmount.multiply(PERCENTAGE_TIER_ONE);
-				
-				finalAmount = grossAmount.subtract(tax);
-				System.out.println("Your tax is: " + tax);
-			} else if ( grossAmount.compareTo(secondTier) == 1 &&
-						(grossAmount.compareTo(thirdTier) == 0 ||
-						grossAmount.compareTo(thirdTier) == -1) ) {
-	
-				BigDecimal firstTaxableAmount = new BigDecimal("31785");
-				BigDecimal secondTaxableAmount = grossAmount.subtract(secondTier);
-				
-				// Calculating tax if amount is between 42,385 and 150,000
-				
-				BigDecimal tax = (firstTaxableAmount.multiply(
-								  PERCENTAGE_TIER_ONE)).add(
-								  secondTaxableAmount.multiply(
-								  PERCENTAGE_TIER_TWO));
-				
-				finalAmount = grossAmount.subtract(tax);
-				System.out.println("Your tax is: " + tax);
-				
-			} else if ( grossAmount.compareTo(thirdTier) == 1 ) {
-				
-				BigDecimal firstTaxableAmount = new BigDecimal("31785");
-				BigDecimal secondTaxableAmount = thirdTier.subtract(secondTier);
-				BigDecimal thirdTaxableAmount = finalTier.subtract(grossAmount);
-				
-				// Calculating tax if amount is between 150,000 and 200,000
-				
-				BigDecimal tax = (firstTaxableAmount.multiply(
-								  PERCENTAGE_TIER_ONE).add(
-								  secondTaxableAmount.multiply(
-								  PERCENTAGE_TIER_TWO)).add(
-								  thirdTaxableAmount.multiply(
-								  PERCENTAGE_TIER_THREE)));
-				
-				finalAmount = grossAmount.subtract(tax);
-				System.out.println("Your tax is: " + tax);
-			}
+			//Calculating tax of 0 %
+
+			finalAmount = grossAmount;
+
+		} else if (grossAmount.compareTo(basicRateStart) == 1 && 
+				(grossAmount.compareTo(higherRateStart) == 0 ||
+				grossAmount.compareTo(higherRateStart) == -1) ) {
+
+			BigDecimal taxableAmount = grossAmount.subtract(basicRateStart);
+
+			// Calculating tax for basic rate of 20%
+
+			BigDecimal tax =  taxableAmount.multiply(BASIC_RATE);
+
+			finalAmount = grossAmount.subtract(tax);
+			System.out.println("Your tax is: " + tax);
+		} else if ( grossAmount.compareTo(higherRateStart) == 1 &&
+				(grossAmount.compareTo(additionalRateStart) == 0 ||
+				grossAmount.compareTo(additionalRateStart) == -1) ) {
+
+			BigDecimal firstTaxableAmount = new BigDecimal("31865");
+			BigDecimal secondTaxableAmount = grossAmount.subtract(higherRateStart);
+
+
+			// Calculating higher tax rate of 40%
+
+			BigDecimal tax = ((firstTaxableAmount.multiply(
+					BASIC_RATE)).add(
+							secondTaxableAmount.multiply(
+									HIGHER_RATE)));
+
+			finalAmount = grossAmount.subtract(tax);
+		} else if ( grossAmount.compareTo(additionalRateStart) == 1 ) {
+
+			BigDecimal firstTaxableAmount = new BigDecimal("31865");
+			BigDecimal secondTaxableAmount = additionalRateStart.subtract(higherRateStart);
+			BigDecimal thirdTaxableAmount = limit.subtract(grossAmount);
+
+			// Calculating additional tax rate of 45%
+
+			BigDecimal tax = (firstTaxableAmount.multiply(
+					BASIC_RATE).add(
+							secondTaxableAmount.multiply(
+									HIGHER_RATE).add(
+											thirdTaxableAmount.multiply(
+													ADDITIONAL_RATE))));
+
+			finalAmount = grossAmount.subtract(tax);
 		}
 		return finalAmount;
 	}
+
 }
+
+
+
+
