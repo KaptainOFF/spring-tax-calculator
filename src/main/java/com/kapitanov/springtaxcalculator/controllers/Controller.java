d2c8b13f5bbdfcd42e9af7b74cb84f0145901ede
yani.kapitanov <yani.kapitanov@gmail.com>
2016-10-12 13:35:53 +0100

    Better Validation handling

--- src/main/java/com/kapitanov/springtaxcalculator/controllers/Controller.java
+++ src/main/java/com/kapitanov/springtaxcalculator/controllers/Controller.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kapitanov.springtaxcalculator.model.UserData;
@@ -21,11 +23,11 @@ import com.kapitanov.springtaxcalculator.services.TaxCalculatorService;
 @RestController
 @RequestMapping("/tax-calculator")
 public class Controller {
-	
+
 	@Autowired
 	private TaxCalculatorService taxCalcService;
-	
-	@RequestMapping(value="/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
 	public ResponseEntity<UserData> getInstance(@PathVariable long id) {
 		UserData user = taxCalcService.findById(id);
 		if (user == null) {
@@ -33,16 +35,20 @@ public class Controller {
 		}
 		return ResponseEntity.ok(user);
 	}
+
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<UserData> postData(@Validated @RequestParam("email") String email,
-						@Validated @RequestParam("amount") BigDecimal takeHomePay,
-						@Validated @RequestParam("taxYear") String taxYear,
-						@Validated HttpServletRequest request) {
-		try {
-			Long id = taxCalcService.save(email,taxYear,takeHomePay,request.getRemoteAddr());
-			return ResponseEntity.ok(taxCalcService.findById(id));
-		} catch (ConstraintViolationException cve) {
-			return new ResponseEntity<UserData>(HttpStatus.BAD_REQUEST);
-		}
+			@Validated @RequestParam("amount") BigDecimal takeHomePay,
+			@Validated @RequestParam("taxYear") String taxYear, @Validated HttpServletRequest request) {
+
+		Long id = taxCalcService.save(email, taxYear, takeHomePay, request.getRemoteAddr());
+		return ResponseEntity.ok(taxCalcService.findById(id));
+
+	}
+
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(ConstraintViolationException.class)
+	public String handleValidationException(ConstraintViolationException cve) {
+		return cve.getMessage();
 	}
 }
